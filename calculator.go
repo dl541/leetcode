3191a8b7ad71935b893c03f01b177798c7846daf
dl541 <davelei97mathsg@gmail.com>
2024-03-07 20:44:20 -0800

    Completed leetcode calculator implementation

--- calculator.go
+++ calculator.go
@@ -2,17 +2,20 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 	"unicode"
 )
 
 func main() {
-	calculate("3+5/5*4/(4-32)")
+	ans := calculate("3+5/(5-4)")
+	fmt.Println(ans)
 }
 
 func calculate(s string) int {
+	tokens := tokenize(&s)
 	fmt.Printf("%v\n", tokenize(&s))
-	return 0
+	return NewScanner(tokens).parseTerm()
 }
 
 type Scanner struct {
@@ -22,21 +25,77 @@ type Scanner struct {
 
 func NewScanner(tokens []string) *Scanner {
 	return &Scanner{
-		tokens: tokens,
+		tokens: append(tokens, "#"),
 		ind:    0,
 	}
 }
 
 func (s *Scanner) parseTerm() int {
-	if s[ind] == 
+	val := s.parseFactor()
+
+loop:
+	for {
+		switch s.lookup() {
+		case "+":
+			s.consume()
+			val += s.parseFactor()
+		case "-":
+			s.consume()
+			val -= s.parseFactor()
+		default:
+			break loop
+		}
+	}
+	return val
 }
 
 func (s *Scanner) parseFactor() int {
+	val := s.parseItem()
 
+loop:
+	for {
+		switch s.lookup() {
+		case "*":
+			s.consume()
+			val *= s.parseItem()
+		case "/":
+			s.consume()
+			val /= s.parseItem()
+		default:
+			break loop
+		}
+	}
+	return val
 }
 
 func (s *Scanner) parseItem() int {
-	
+	fmt.Printf("%+v %v %v\n", s, s.ind, s.lookup())
+
+	if s.lookup() == "(" {
+		s.consume()
+		val := s.parseTerm()
+		if s.lookup() == ")" {
+			s.consume()
+		} else {
+			panic(fmt.Sprintf("Expect ), but got %v in ind %v", s.lookup(), s.ind))
+		}
+		return val
+	} else {
+		val, err := strconv.Atoi(s.lookup())
+		if err != nil {
+			panic(fmt.Sprintf("Expect digits, but got %v in ind %v", s.lookup(), s.ind))
+		}
+		s.consume()
+		return val
+	}
+}
+
+func (s *Scanner) lookup() string {
+	return s.tokens[s.ind]
+}
+
+func (s *Scanner) consume() {
+	s.ind++
 }
 
 func tokenize(s *string) []string {
@@ -49,5 +108,5 @@ func tokenize(s *string) []string {
 		}
 	}
 
-	return strings.Split(strings.Join(tokens, ""), " ")
+	return strings.Fields(strings.Join(tokens, ""))
 }
